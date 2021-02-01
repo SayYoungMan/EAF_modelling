243bf37b189624d7a670fb0974aecdfa04c7085b
SayYoungMan <nickte89@gmail.com>
2021-02-01 02:32:29 +0000

    First trial of removing some equations

--- trial.m
+++ trial.m
@@ -1,3 +1,5 @@
-fun = @eaf;
-x0 = zeros(1, 130);
-eaf(x0)
+fun = @eaf_improved;
+x0 = linspace(1, 28, 28);
+options = optimset('disp', 'iter', 'LargeScale', 'off', 'TolFun', .001 ...
+    , 'MaxIter', 10000000, 'MaxFunEvals', 100000);
+dx = fsolve(fun, x0, options);
